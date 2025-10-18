3e6608ae1983583a01da91037f32960af7298e39
Leon Rohne <leon.rohne@gmx.com>
2025-10-18 11:57:22 +0200

    Rook isLegalMove (not finished)

--- Rook.java
+++ Rook.java
@@ -19,9 +19,59 @@ public class Rook extends Piece{
         char currentColumn = this.getTile().getColumn();
         int rowIndex = currentRow -1;
         int columnIndex = currentColumn - 'A';
+        boolean noAllyPiecesInWayVertical = true;
+        boolean noAllyPiecesInWayHorizontal = true;
 
+        if(currentRow < targetRow) {
+            for (int i = currentRow; i < targetRow - 1; i++) {
+                if (Objects.equals(Game.board[i][columnIndex].getPiece().getColour(), this.getColour())) {
+                    noAllyPiecesInWayVertical = false;
+                    break;
+                }
+            }
+        }
+        if(currentRow > targetRow) {
+            for (int i = targetRow; i < currentRow - 1; i++) {
+                if (Objects.equals(Game.board[i][columnIndex].getPiece().getColour(), this.getColour())) {
+                    noAllyPiecesInWayVertical = false;
+                    break;
+                }
+            }
+        }
+        if(currentColumn < targetColumn) {
+            for (char i = currentColumn; i < targetColumn - 'A'; i++) {
+                if (Objects.equals(Game.board[rowIndex][i].getPiece().getColour(), this.getColour())) {
+                    noAllyPiecesInWayHorizontal = false;
+                    break;
+                }
+            }
+        }
+        if(currentColumn > targetColumn) {
+            for (char i = targetColumn; i < currentColumn - 'A'; i++) {
+                if (Objects.equals(Game.board[rowIndex][i].getPiece().getColour(), this.getColour())) {
+                    noAllyPiecesInWayHorizontal = false;
+                    break;
+                }
+            }
+        }
 
+        if(targetTile == null){
+            return false;
+        }
+        //für vertikale Züge
+        if(!Objects.equals(targetTile.getPiece().getColour(), this.getColour())
+                && targetColumn == currentColumn
+                && noAllyPiecesInWayVertical){
+            return true;
+        }
+        //für horizontale Züge
+        if(!Objects.equals(targetTile.getPiece().getColour(), this.getColour())
+                && targetRow == currentRow
+                && noAllyPiecesInWayHorizontal){
+            return true;
+        }
 
+        //Schmeißen noch implementieren (noEnemyPiecesInWayVertical/noEnemyPiecesInWayHoriontal)?
             return false;
     }
 }
